3e77fd7a4751648615a2af158860dde1029b7a83
Urmatskiy <nickiton14@gmail.com>
2023-04-01 09:41:52 +0300

    Выполнил второе задание

--- program.java
+++ program.java
@@ -1,6 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class program {
     public static void main(String[] args) {
         System.out.println(buildString(6, 'a', 'b'));
+
+        createFile("TEXT");
     }
 
     static String buildString(int n, char c1, char c2) {
@@ -14,4 +21,25 @@ public class program {
         return result;
     }
 
+    static void createFile(String input) {
+        Path path = Path.of("file.txt");
+        
+        try {
+            Files.createFile(path);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String result = "";
+        for (int i = 0; i < 100; i++) {
+            result += input;
+        }
+
+        try {
+            Files.writeString(path, result);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    
+}
 }
